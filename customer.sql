9afd49b6ede9c432650eb4b3e1a20db5b57a98e6
Pallav Sharma <53930841+pallav04091997@users.noreply.github.com>
2019-10-30 11:33:03 +0530

    Add files via upload

---
+++ customer.sql
@@ -0,0 +1,6 @@
+create table customers(
+c_id int not null ,
+c_name varchar(100) not null,
+order_date date not null,
+order_place varchar(100),
+d_reference_id int);
---
+++ delieveries.sql
@@ -0,0 +1,5 @@
+create table delieveries(
+d_id int not null auto_increment primary key,
+d_name varchar(100) not null,
+avg_rating decimal(2,1) not null,
+number bigint not null);
